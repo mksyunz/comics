b67081b961d12ee2e14206e90c5efad05d6dc038
Max Ksyunz <mksyunz@gmail.com>
2011-04-05 00:49:50 -0700

    Initial commit of the comics web app.

---
+++ story-data.js
@@ -0,0 +1,24 @@
+{	"comics": "Dresden Codak",
+	"storylines": 
+	[	{	"title": "Dark Science",
+			"pages": 
+				[ "http://dresdencodak.com/comics/2010-06-03-dark_science_01.jpg"
+				, "http://dresdencodak.com/comics/2010-06-10-dark_science_02.jpg"
+				, "http://dresdencodak.com/comics/2010-07-10-dark_science_03.jpg"
+				, "http://dresdencodak.com/comics/2010-07-26-2010-7-16-dark_science_04.jpg"
+				, "http://dresdencodak.com/comics/2010-09-25-dark_science_05.jpg"
+				]
+		}
+		, { "title": "Hob",
+			"pages":
+				[ "http://dresdencodak.com/comics/2007-02-08-pom.jpg"
+				, "http://dresdencodak.com/comics/2007-02-23-billions_and_billions.jpg"
+				, "http://dresdencodak.com/comics/2007-04-07-rule_110.jpg"
+				, "http://dresdencodak.com/comics/2007-05-06-after_many_a_summer_dies_the_swan.jpg"
+				, "http://dresdencodak.com/comics/2007-05-22-for_lack_of_a_better_term.jpg"
+				, "http://dresdencodak.com/comics/2007-06-12-the_witching_hour.jpg"
+				, "http://dresdencodak.com/comics/2007-06-30-in_the_company_of_science.jpg"
+				]
+		}
+	]
+}
